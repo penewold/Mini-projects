6efe896b0e46ee6f770d0f076d6af3a8ce8ed5dd
penewold <nicolas.enoe@gmail.com>
2023-09-01 07:12:34 +0200

    added model choosing

--- 3d/draw-test.py
+++ 3d/draw-test.py
@@ -1,6 +1,7 @@
 import pygame as pg
 import numpy as np
-
+import tkinter as tk
+from tkinter import filedialog
 
 
 def handle_vertex_args(args):
@@ -8,6 +9,7 @@ def handle_vertex_args(args):
 
 
 vertices = []
+
 
 def parse_model(model):
     #model_arguments = []
@@ -28,8 +30,10 @@ def parse_model(model):
     
     
     
-
-parse_model("fish.obj")
+root = tk.Tk()
+root.withdraw()
+file_path = filedialog.askopenfilename()
+parse_model(file_path)
 print(vertices)
 
 pg.init()
@@ -37,6 +41,9 @@ screen = pg.display.set_mode((1280, 720), pg.RESIZABLE)
 width, height = pg.display.get_window_size()
 clock = pg.time.Clock()
 running = True
+model_size = 400
+dot_size = 3
+location = [200, 200]
 
 while running:
     # poll for events
@@ -44,14 +51,27 @@ while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
-
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_UP:
+                model_size += 10
+            if event.key == pg.K_DOWN:
+                model_size -= 10
+            if event.key == pg.K_w:
+                dot_size += 1
+            if event.key == pg.K_s:
+                dot_size -= 1
+            if event.key == pg.K_d:
+                location[0] += 20
+            if event.key == pg.K_a:
+                location[1] -= 20
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
 
     for vertex in vertices:
         #print(vertices)
         #print(vertex)
-        pg.draw.circle(screen, (255, 255, 255), ((vertex[0]) * 400 + 200, height - ((vertex[1]) * 400 + 200)), 3)
+        pg.draw.circle(screen, (255, 255, 255), ((vertex[0]) * model_size + location[0], height - ((vertex[1]) * model_size + location[1])), dot_size)
+
 
     # flip() the display to put your work on screen
     pg.display.flip()
